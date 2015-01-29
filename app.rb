ce705e8da01dfc15e6d7cbc18ebfa6053fd44a9e
David Zulaica & Grace Mekarski <student@epicodus.com>
2015-01-29 11:40:51 -0800

    Add initial Sinatra components for Recipe class.

--- app.rb
+++ app.rb
@@ -2,3 +2,14 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get("/") do
+  @recipes = Recipe.all()
+  erb(:index)
+end
+
+post("/recipes") do
+  recipe_title= params.fetch('recipe_title')
+  Recipe.create({:title=> recipe_title, :instructions => "", :rating => 0})
+  redirect('/')
+end
